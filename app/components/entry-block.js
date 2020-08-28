bbbffaa7e5fabc02a6578cc77a19fa51a8c9f432
Stacy Harrison <stacy.e.harrison@gmail.com>
2020-08-27 21:36:06 -0400

    Add date helpers

--- app/components/entry-block.js
+++ app/components/entry-block.js
@@ -6,10 +6,4 @@ export default Component.extend({
   tagName: 'article',
   classNames: ['tile', 'is-child', 'box'],
   entry: null,
-
-  displayCreatedAt: computed('entry', function() {
-    if (moment(this.entry.createdAt).isValid()) {
-      return moment(this.entry.createdAt).fromNow();
-    }
-  }),
 });
